ae802db369532e9a6c9ac65778fae30e37f67ce8
Riany Mello <mello.riany@gmail.com>
2023-06-05 18:47:57 +0100

    :tada: first commit

---
+++ src/pages/Banner.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Banner.module.css';
+
+const Banner = () => {
+  return (
+    <div className={styles.banner}>
+      <h1 className={styles.bannerTitle}>Seu Título Aqui</h1>
+    </div>
+  );
+};
+
+export default Banner;
