a04d1c749b3d7cb6a141272de025252fd24298cd
Riany Mello <mello.riany@gmail.com>
2024-02-07 14:40:09 +0000

    a

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,12 +5,7 @@ const Home = () => {
   return (
     <div>
    
-   <Image
-      src={Croco}
-      alt="Crocodilo"
-      width={100}
-      height={70}
-   />
+   <iframe width="560" height="315" src="https://www.youtube.com/embed/9S5PzKGelgY?si=9eo2piJX1C81IiPn" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   );
 }
---
+++ src/pages/banner.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div>
+        header
+     </div>
+    );
+  }
+  
+  export default Home;
+  
